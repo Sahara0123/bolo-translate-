55f028653b37e664e0263796896b183c9a13588f
Sahara0123 <vishnuchahar006@gmail.com>
2025-06-23 22:52:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,14 @@
-const languages = {
-  "en": "English",
-  "hi": "Hindi",
-  "es": "Spanish",
-  "fr": "French",
-  "de": "German",
-  "zh": "Chinese",
-  "ar": "Arabic"
-};
-
-window.onload = () => {
-  const fromLang = document.getElementById("fromLang");
-  const toLang = document.getElementById("toLang");
-  for (let code in languages) {
-    fromLang.innerHTML += `<option value="${code}">${languages[code]}</option>`;
-    toLang.innerHTML += `<option value="${code}">${languages[code]}</option>`;
-  }
-  fromLang.value = "en";
-  toLang.value = "hi";
-};
 
 function startVoiceInput() {
-  const recognition = new webkitSpeechRecognition();
-  recognition.lang = document.getElementById("fromLang").value;
-  recognition.onresult = (e) => {
-    document.getElementById("inputText").value = e.results[0][0].transcript;
-    translateText();
-  };
-  recognition.start();
+  alert("Voice input feature will activate here.");
 }
 
 function speakText() {
-  const utterance = new SpeechSynthesisUtterance(document.getElementById("outputText").value);
-  utterance.lang = document.getElementById("toLang").value;
-  speechSynthesis.speak(utterance);
+  alert("Speaking translated text...");
 }
 
-$('#inputText').on('input', translateText);
-$('#fromLang, #toLang').on('change', translateText);
-
-function translateText() {
-  const text = $('#inputText').val();
-  const from = $('#fromLang').val();
-  const to = $('#toLang').val();
-  if (!text) return;
-  const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${from}&tl=${to}&dt=t&q=${encodeURIComponent(text)}`;
-  $.get(url, function(data) {
-    $('#outputText').val(data[0][0][0]);
-  });
+function switchLanguages() {
+  let from = document.getElementById("fromLang");
+  let to = document.getElementById("toLang");
+  [from.value, to.value] = [to.value, from.value];
 }
