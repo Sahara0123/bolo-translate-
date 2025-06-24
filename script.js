e9331e2c309034692be377fd255c9c11d1b09d40
Sahara0123 <vishnuchahar006@gmail.com>
2025-06-25 02:59:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-
-function startVoiceInput() {
-  alert("Voice input feature will activate here.");
-}
-
-function speakText() {
-  alert("Speaking translated text...");
-}
-
-function switchLanguages() {
-  let from = document.getElementById("fromLang");
-  let to = document.getElementById("toLang");
-  [from.value, to.value] = [to.value, from.value];
-}
+console.log('Bolo Translate active');
